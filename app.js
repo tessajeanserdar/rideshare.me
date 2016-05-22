ce8890d61a96179a3b0f597e98d761e274c344d9
Tessa Serdar <tessajeanserdar@gmail.com>
2016-05-21 17:47:47 -0700

    Sets Oauth callback to deployed url

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var uber = new Uber({
   client_id: 'qfa9685JhoyhkXKToPtOo5ViSHufieac',
   client_secret: '1Ib94U_m02pgWZnL_jmYheKik5t80ytI3eVg32o5',
   server_token: '8dVsmxZH9Xe8KRPBUONd4s4dsyYpgXZf_70Fegpa',
-  redirect_uri: 'http://localhost:3000/api/callback',
+  redirect_uri: 'https://hidden-sea-86580.herokuapp.com/api/callback',
   name: 'RideStats.me',
   language: 'en_US', // optional, defaults to en_US
   sandbox: true // optional, defaults to false
